c58f6fcd95a7b3806c0b55471b168e7a7254a5d3
iambb5445 <iambb5445@yahoo.com>
2022-01-30 22:40:43 -0800

    Remove skip for night!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -71,35 +71,35 @@ public class GameManager : MonoBehaviour
         new Day(35, "L1", "You can drag and throw sheeps behind the fences.\nThe timer shows when the night comes."),
         new Night(20, "You can't move sheeps when night comes."),
         new Notif(10, "Wolves are around.", "Don't let the wolves kill the sheeps."),
-        new Day(30, "L2", "You can skip day or night or text by pressing L."),
-        new Night(30, "Wolves are faster at night.\nRemember, you can't move the sheeps when the night comes."),
+        new Day(30, "L2", "You can skip day or text by pressing L."),
+        new Night(20, "Wolves are faster at night.\nRemember, you can't move the sheeps when the night comes."),
         new Notif(10, "The shepherd has an ability! Not so useful yet...", "Use Left Shift to temporarily change the time to night."),
         new Day(30, "L3", "Wolves are not faster at temporarily night."),
-        new Night(30),
+        new Night(20),
         new Notif(5, "Some nights are hard...", "Throwing a sheep is faster than carrying it."),
         new Day(30, "L4", "Throwing a sheep is faster than carrying it."),
-        new Night(30),
+        new Night(20),
         new Notif(15, "One night, the shepherd finds out some sheeps are cursed!\nThe Curse of Duality!", "Some sheeps will change into wolves at night."),
         new Day(30, "L5", "You can temporarily change the time to night\nand see which sheeps are going to change to wolves.\nBut they can still kill other's at temporary nights!"),
-        new Night(30, "You can use Left Shift to temporarily change the time to night"),
+        new Night(20, "You can use Left Shift to temporarily change the time to night"),
         new Notif(15, "", "Try to remember which sheeps are wolves instead of constantly using temporary night."),
         new Day(30, "L6"),
-        new Night(30),
+        new Night(20),
         new Notif(15, "", "Find a way!"),
         new Day(30, "L7"),
-        new Night(30),
-        new Notif(15, "To make things worst, some wolves also change into sheeps at night!\nBut you are too kind hearted to let them die...", "Save them all!"),
+        new Night(20),
+        new Notif(15, "To make things worst, some wolves also change into sheeps at night!\nBut the shepherd is too kind hearted to let them die...", "Save them all!"),
         new Day(30, "L8"),
-        new Night(30),
+        new Night(20),
         new Notif(15, "", "Throw!"),
         new Day(40, "L90"),
-        new Night(30),
+        new Night(20),
         new Notif(15, "", "Very confusing..."),
         new Day(30, "L10"),
-        new Night(30),
+        new Night(20),
         new Notif(15, "", "Good luck!"),
         new Day(30, "L11"),
-        new Night(30),
+        new Night(20),
     };
 
     float timer = 0f;
@@ -124,7 +124,7 @@ public class GameManager : MonoBehaviour
         {
             return;
         }
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.L) && levelIndex < levels.Length && levels[levelIndex].levelType != Level.LevelType.Night)
         {
             timer = 0;
         }
@@ -160,7 +160,7 @@ public class GameManager : MonoBehaviour
             if (levelIndex >= levels.Length)
             {
                 notifObject.SetActive(true);
-                notifText.text = "<color=yellow>Score:" + aliveCount + " out of " + total + "animals saved!</color>";
+                notifText.text = "<color=yellow>Score: " + aliveCount + " out of " + total + " animals saved!</color>";
                 SceneManager.LoadScene("Notif");
                 ended = true;
             }
